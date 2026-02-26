14d0a197b0260442a3f4251a85a1567131b0872e
olivyao <oliv28yao@gmail.com>
2026-02-25 22:33:13 -0500

    updates

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,10 +6,41 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     MyInput_json = { "raw_document": { "text": text_to_analyze } }
 
-    response = requests.post(URL, headers = headers, json = MyInput_json)
+    try:
+        response = requests.post(URL, headers = headers, json = MyInput_json)
+
+    except (requests.exceptions.RequestException):
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
     #Convert response to dictionary
     dict_response = json.loads(response.text)
+
+    if 'emotionPredictions' not in  dict_response:
+         return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
     #extraction of emotion scores from response
     emotions = dict_response['emotionPredictions'][0]['emotion']
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Flask emotion detection
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,33 +9,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    render main page
+    """
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def emotion_analyzer():
-    text_to_analyze = request.args.get('textToAnalyze')  
+    """
+    Analyze the emotion
+    """
+    text_to_analyze = request.args.get('textToAnalyze')
+    if not text_to_analyze or not text_to_analyze.strip():
+        return "Invalid text! Please try again!"
 
     result = emotion_detector(text_to_analyze)
-
     #extract emotions
     anger = result['anger']
     disgust = result['disgust']
     fear = result['fear']
     joy = result['joy']
     sadness = result['sadness']
-    dominant = result['dominant_emotion'] 
-
+    dominant = result['dominant_emotion']
     response = (
         f"For the given statement, the system response is "
-        f"'anger': {anger}"
-        f"'disgust': {disgust}"
-        f"'fear': {fear}"
-        f"'joy': {joy}"
+        f"'anger':{anger}"
+        f"'disgust':{disgust}"
+        f"'fear':{fear}"
+        f"'joy':{joy}"
         f"'sadness': {sadness}"
         f"'The dominant emotion is': {dominant}"
     )
-
     return response
 
-if __name__ == '__main__' : 
-    app.run(host='0.0.0.0', port=5000, debug=True)    
+if __name__ == '__main__' :
+    app.run(host='0.0.0.0', port=5000, debug=True)
